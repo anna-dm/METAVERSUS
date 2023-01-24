4400afe07433f81749e72e96a67b1a5a31dce82d
anna-dm <anna-anna-frolova@bk.ru>
2023-01-24 23:54:15 +0300

    get started

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,41 @@
 'use client';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { startingFeatures } from '../constants';
 
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={planetVariants('left')}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title=" | Hoe metaversus Works"
+
+      </motion.div>
+
+    </motion.div>
   </section>
 );
 
