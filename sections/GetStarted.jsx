28f9f1d341f01e59964ac34b21f9a8a5acafdec7
anna-dm <anna-anna-frolova@bk.ru>
2023-01-25 22:10:36 +0300

    new section

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -31,10 +31,22 @@ const GetStarted = () => (
         variants={fadeIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
-        <TypingText title=" | Hoe metaversus Works"
+        <TypingText title=" | How metaversus Works" />
+        <TitleText title={<>Get started with just a few clicks</>} />
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps
+              key={feature}
+              number={index + 1}
+              text={feature}
+            />
+
+          ))}
+
+
+        </div>
 
       </motion.div>
-
     </motion.div>
   </section>
 );
